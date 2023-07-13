aa3a86891c5688a6fadb18ebc465947f8d63096e
Josh <joshp1@joshlucy.info>
2023-07-13 14:30:55 -0400

    Aded indexes to the other folders

---
+++ stories/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+    <head>
+        <title>
+            Aohd stories and comics
+        </title>
+    </head>
+    <script type="module" src="https://md-block.verou.me/md-block.js"></script>
+    <style language = "text/css">
+        #hdr 
+        {
+            background-color:grey;
+        }
+        #hdr a
+        {   color:white;
+
+            text-decoration: none;
+        }
+    </style>
+    <body>
+        <div id = "hdr">
+<?php
+
+if ($handle = opendir('.')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != ".." && $entry !="index.php") {
+
+            echo "<a href='$entry'>$entry</a>|\n";
+        }
+    }
+
+    closedir($handle);
+}?>
+</div>
+<md-block>
+<?php
+    $file = fopen ("ideas", 'r');
+
+    // Outpu lines until EOF is reached
+    while (! feof ($file)) {
+        $line = fgets ($file);
+        print $line;
+    }
+
+    fclose ($file);?>
+</md-block>
+
+</body></html>
