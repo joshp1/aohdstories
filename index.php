142d3b5598dd5a3c14ad24e0eab694e3f608be69
Josh <joshp1@joshlucy.info>
2023-07-14 02:46:59 -0400

    readed bottom  border fixed now

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         #hda {
             background-color :grey;width:63px;
             display:block;float:left;
-            bottom :2px solid;border-left :2px solid;border-top :2px solid;
+            border-bottom :2px solid;border-left :2px solid;border-top :2px solid;
         }
         #hdb
         {
